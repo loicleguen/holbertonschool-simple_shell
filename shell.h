91d74eaadd333a235ddbea51710debbad17e9adf
Boubouche1709 <hadjarflorian@gmail.com>
2025-08-18 13:47:55 +0200

    Fix prototypes in header file

--- shell.h
+++ shell.h
@@ -30,6 +30,8 @@ typedef struct command_s
 ssize_t read_command(char **input, size_t *bufsize, int is_interactive);
 char **parse_command(char *line);
 int execute_command(command_t cmd);
+char *find_command_in_path(char *command);
+char *_strdup(char *str);
 int main(void);
 
 #endif /* SHELL_H */
